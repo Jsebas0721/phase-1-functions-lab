248820d9a7fae919ddb8c5769cd621d0c865a09b
sebastian medina <sebastianlopez_21@hotmail.com>
2022-08-16 10:08:37 -0400

    commit tree 2132a304525a013c730e2e2a96e7d952896d93d5

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    
+    let hqLocation = 42;
+
+    if(someValue > hqLocation){
+        return someValue - hqLocation;
+    }else{
+        return hqLocation - someValue;
+    }
+
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)* 264;
+}
+
+
+function distanceTravelledInFeet(start, destination){
+    
+    let maxStreetNum = Math.max(start, destination);
+    let minStreetNum = Math.min(start, destination);
+
+    return (maxStreetNum - minStreetNum) * 264;
+
+}
+
+function calculatesFarePrice(start, destination){
+    
+    let distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if(distanceInFeet < 400){
+        return 0;
+    }else if(distanceInFeet >= 400 && distanceInFeet <= 2000){
+        return (distanceInFeet - 400)*.02;
+    }else if(distanceInFeet > 2000 && distanceInFeet <= 2500){
+        return 25;
+    }else if(distanceInFeet > 2500){
+        return 'cannot travel that far';
+    }
+  
+}
